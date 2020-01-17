92a577f8f8485868ab3213f57a1d38b68578f945
Clinton <c.bestmann@hotmail.com>
2020-01-17 14:40:35 -0800

    Working API

--- RequestHeaderParser/Startup.cs
+++ RequestHeaderParser/Startup.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,11 +30,10 @@ namespace RequestHeaderParser
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
@@ -41,18 +42,34 @@ namespace RequestHeaderParser
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    var html = File.ReadAllText(Directory.GetCurrentDirectory() + "/Views/index.html");
+                    using var fs = new FileStream(Directory.GetCurrentDirectory() + "/Views/index.html", FileMode.Open);
                     context.Response.ContentType = "text/html; charset=utf-8";
-                    await context.Response.WriteAsync(html, Encoding.UTF8);
+                    await fs.CopyToAsync(context.Response.Body);
                 });
 
-                endpoints.MapControllers();
+                endpoints.MapGet("/api/whoami", async context =>
+                {
+                    dynamic responseObj = new System.Dynamic.ExpandoObject();
+                    responseObj.ipaddress = context.Connection.RemoteIpAddress.MapToIPv4().ToString();
+                    var headers = context.Request.Headers;
+                    if (headers.TryGetValue("Accept-Language", out var langVal))
+                    {
+                        responseObj.language = langVal.ToString();
+                        logger.LogInformation(langVal);
+                    }
+                    if (headers.TryGetValue("User-Agent", out var uaVal))
+                    {
+                        responseObj.software = uaVal.ToString();
+                        logger.LogInformation(uaVal);
+                    }
+                    string objStr = JsonSerializer.Serialize(responseObj);
+                    context.Response.ContentType = "application/json";
+                    await context.Response.WriteAsync(objStr);
+                });
             });
         }
     }
